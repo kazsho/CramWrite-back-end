6fdc227271980ecfbc3244421e025303894332a2
henrietta adeniran <henrietta.akindele@gmail.com>
2024-03-06 09:40:55 +0000

    amend create and update method

--- api/models/Client.js
+++ api/models/Client.js
@@ -24,18 +24,19 @@ static async getOneById(id) {
 }
 
 static async create(body) {
-    const {client, is_teacher, username, password} = body;
-    const response = await db.query('INSERT INTO client (client, is_teacher, username, password) VALUES ($1, $2, $3, $4) RETURNING *;', [client, is_teacher, username, password]);
+    const {client, teacher, username, password} = body;
+    const response = await db.query('INSERT INTO client (client, is_teacher, username, password) VALUES ($1, $2, $3, $4) RETURNING *;', [client, teacher, username, password]);
 
     return new Client(response.rows[0]);
 }
 
 async update(body) {
-    const {client, is_teacher, username, password} = body;
-    if (!client || !is_teacher || !username || !password) {
+    const {client, teacher, username, password} = body;
+    console.log(teacher)
+    if (!client || (!teacher && teacher !== false) || !username || !password) {
         throw new Error("Missing Data!");
     };
-    const response = await db.query('UPDATE client SET client = $1, is_teacher = $2, username = $3, password = $4 WHERE client_id = $5 RETURNING *;', [client, is_teacher, username, password, this.id]);
+    const response = await db.query('UPDATE client SET client = $1, is_teacher = $2, username = $3, password = $4 WHERE client_id = $5 RETURNING *;', [client, teacher, username, password, this.id]);
     return new Client(response.rows[0]);
 }
 
